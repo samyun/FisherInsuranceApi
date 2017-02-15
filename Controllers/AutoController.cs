b5f3fe8deaf48eddda2d3c5af3e0881316986b2f
Samuel Yun <samyun95@gmail.com>
2017-02-15 17:10:22 -0500

    Add MemoryStore to AutoController

--- Controllers/AutoController.cs
+++ Controllers/AutoController.cs
@@ -1,33 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
+using FisherInsuranceApi.Data;
+using FisherInsuranceApi.Models;
 
 [Route("api/auto/quotes")]
 public class AutoController : Controller
 {
+    private IMemoryStore db; 
+    public AutoController(IMemoryStore repo) 
+    {
+        db = repo;
+    }
     // POST api/auto/quotes
     [HttpPost]
-    public IActionResult Post([FromBody]string value)
+    public IActionResult Post([FromBody] Quote quote)
     {
-        return Created("", value);
+        return Ok(db.CreateQuote(quote));
     }
 
     // GET api/auto/quotes/5
     [HttpGet("{id}")]
     public IActionResult Get(int id)
     {
-         return Ok("The id is: " + id);
+         return Ok(db.RetrieveQuote(id));
     }
 
     // PUT api/auto/quotes/id
     [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody]string value)
+    public IActionResult Put([FromBody] Quote quote)
     {
-        return NoContent();
+        return Ok(db.UpdateQuote(quote));
     }
 
     // DELETE api/auto/quotes/id
     [HttpDelete("{id}")]
     public IActionResult Delete(int id)
     {
-        return Delete(id);
+        db.DeleteQuote(id);
+        return Ok();
+    }
+
+
+    [HttpGet]
+    public IActionResult GetQuotes()
+    {
+        return Ok(db.RetrieveAllQuotes);
     }
 }
