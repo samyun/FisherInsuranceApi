2aeaddb2b5505fdef39cc12994b4f3f52c8eb0b1
Samuel Yun <samyun95@gmail.com>
2017-02-22 18:11:36 -0500

    Adding the Home Page Component

--- wwwroot/app/app.component.ts
+++ wwwroot/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "fisher-insurance",
-    template: `<h1>Hello Fisher Insurance<h1>`
+    template: `<h1>Hello Fisher Insurance<h1>`,
+    template: `<home-page>`
 })
 export class AppComponent { }
--- wwwroot/app/app.module.ts
+++ wwwroot/app/app.module.ts
@@ -5,10 +5,12 @@ import 'rxjs/Rx';
 
 // import our application component
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
